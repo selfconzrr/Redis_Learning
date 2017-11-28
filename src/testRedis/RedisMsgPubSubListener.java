5579cff1b3d40127d8608ed33b924be0700d17f3
selfconzrr <buptzrr@bupt.edu.cn>
2017-11-28 20:56:06 +0800

    增加了发布/订阅功能的实现

--- src/testRedis/RedisMsgPubSubListener.java
+++ src/testRedis/RedisMsgPubSubListener.java
@@ -1,13 +1,18 @@
 package testRedis;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import redis.clients.jedis.JedisPubSub;
 /*
- * 消息监听器类
+ * 消息监听器类，继承自JedisPubSub，并实现其抽象方法
  */
 public class RedisMsgPubSubListener extends JedisPubSub{
 
+	private static Logger logger = LoggerFactory.getLogger(RedisMsgPubSubListener.class);
 	@Override
 	public void onMessage(String channel, String message) {
+		logger.info("Message received. Channel: {}, Msg: {}", channel, message);
 		System.out.println("channel:" + channel + "receives message :" + message);
 		// 可以根据设置何时取消订阅
         // this.unsubscribe(); // 如果注释掉则会收到三条信息，间隔是5秒
